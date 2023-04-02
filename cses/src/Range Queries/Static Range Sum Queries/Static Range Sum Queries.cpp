44b2d0b9ba14f2c9e53dceb3a6b961c4fb4a8ff7
Elia Soldati <eliasol2005@gmail.com>
2023-04-02 15:13:39 +0200

    Add Static Range Sum Queries from cses

---
+++ cses/src/Range Queries/Static Range Sum Queries/Static Range Sum Queries.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n, q;
+	cin >> n >> q;
+	vector<long long> v(n + 1);
+	v[0] = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		cin >> v[i];
+		v[i] += v[i - 1];
+	}
+	while (q--)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << v[b] - v[a - 1] << "\n";
+	}
+	return 0;
+}
