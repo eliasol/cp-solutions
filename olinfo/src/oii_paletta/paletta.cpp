0631a5e05373f25392be5aa30244a64c49aafcb5
Elia Soldati <eliasol2005@gmail.com>
2023-06-22 10:27:52 +0200

    Rewrite oii_paletta from olinfo

--- olinfo/src/oii_paletta/paletta.cpp
+++ olinfo/src/oii_paletta/paletta.cpp
@@ -1,45 +1,61 @@
-#include <memory.h>
-#include <assert.h>
-#include <stdio.h>
+#include <vector>
 
-int n;
-long long fw[800000];
+int N;
+std::vector<int> fw;
 
-void add(int i, long long v)
+void add(int i, int v)
 {
-	i += 1;
-	for (; i <= n; i += i&(-i)) fw[i] += v;
+	++i;
+	while (i <= N)
+	{
+		fw[i] += v;
+		i += i&-i;
+	}
 }
 
-long long sum(int i)
+int sum(int i)
 {
-	i += 1;
-	long long s = 0;
-	for (; i > 0; i -= i&(-i)) s += fw[i];
-	return s;
+	++i;
+	int ans = 0;
+	while (i > 0)
+	{
+		ans += fw[i];
+		i -= i&-i;
+	}
+	return ans;
 }
 
-long long paletta_sort(int N, int V[]) {
-	for (int i = 0; i < N; ++i)
-		if (i%2 != V[i]%2) return -1;
-
+long long countInv(int N, const std::vector<int>& v)
+{
+	fw.assign(N + 1, 0);
 	long long ans = 0;
-	n = (N+1)/2;
-	int v[n];
-	for (int i = 0; i < N; i+=2) v[i/2] = V[i]/2;
-	for (int i = n-1; i >= 0; --i)
+	for (int i = v.size() - 1; i >= 0; --i)
 	{
 		ans += sum(v[i]);
-		add(v[i],1);
+		add(v[i], 1);
 	}
-	n = N/2;
-	memset(fw,0,(n+1)*sizeof(long long));
-	for (int i = 1; i < N; i+=2) v[i/2] = V[i]/2;
-	for (int i = n-1; i >= 0; --i)
+	return ans;
+}
+
+long long paletta_sort(int N, int V[])
+{
+	::N = N;
+	std::vector<int> v;
+	for (int i = 0; i < N; i += 2)
 	{
-		ans += sum(v[i]);
-		add(v[i],1);
+		if (V[i] % 2)
+			return -1;
+		v.push_back(V[i]);
 	}
+	long long ans = countInv(N, v);
+	v.clear();
+	for (int i = 1; i < N; i += 2)
+	{
+		if (V[i] % 2 == 0)
+			return -1;
+		v.push_back(V[i]);
+	}
+	ans += countInv(N, v);
 	return ans;
 }
 
